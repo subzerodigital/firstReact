9d0ef0dd9336bac0cc9f02f7f9c0a8a6891e54cf
Pengcheng <pengchengcao@hotmail.com>
2016-04-12 23:04:33 +1000

    2nd - tired

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // runs a local dev serve
 var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
 
 
 var config = {
@@ -10,7 +13,9 @@ var config = {
 	devBaseUrl:'http://localhost',
 	paths:{
 		html:'./src/*.html',
-		dist:'./dist'
+		js:'./src/**/*.js',
+		dist:'./dist',
+		mainJs:'./src/main.js'
 	}
 };
 
@@ -36,8 +41,20 @@ gulp.task('html',function(){
 		.pipe(connect.reload());
 });
 
+gulp.task('js',function(){
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error',console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist+'/scripts'))
+		.pipe(connect.reload());
+});
+
+
 gulp.task('watch',function(){
 	gulp.watch(config.paths.html,['html']);
+	gulp.watch(config.paths.js,['js']);
 });
 
 gulp.task('default',['html','open','watch']);
